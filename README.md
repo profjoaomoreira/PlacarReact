d6d59de3bf308db42ac5df27f074e56ec3370655
JoaoAlan <professorjoaomoreira@gmail.com>
2019-12-30 20:51:18 -0300

    versao1_placar

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# PlacarReact
+# React Series
+Este pacote contém os arquivos básicos para iniciar as vídeo aulas
+de React do [Canal V++](https://youtube.com/user/VPlusPlus).
---
+++ src/components/Time.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import ButaoGol from './BotaoGol';
+
+export default class Time extends React.Component {
+  render() {
+    return (
+        <div>
+            <h1>Vasco</h1>
+            <h2>0</h2>
+            <botaoGol />
+        </div>
+        );
+    }
+}
